607799128a5edc5077b6deb774b7ad83724a1141
BlackList <azokblack@gmail.com>
2023-11-05 19:28:56 +0400

    commit branch selenoid add remote

--- src/test/java/pageObject/base/TestBase.java
+++ src/test/java/pageObject/base/TestBase.java
@@ -1,18 +1,55 @@
 package pageObject.base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
-    @BeforeAll
+    /*@BeforeAll
     static void setUp() {
         //Configuration.pageLoadTimeout = 70000;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
+    }*/
+
+    @BeforeAll
+    static void setUp() {
+        //Configuration.pageLoadTimeout = 70000;
+        //Configuration.baseUrl = "https://demoqa.com";
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+
+        //Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //capabilities.setCapability("browserName", "chrome");
+        //capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", false
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 }
