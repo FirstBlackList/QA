bed2dbaed972b7c2400229d701ebaec8ab6280de
BlackList <azokblack@gmail.com>
2023-11-05 19:27:33 +0400

    commit branch simple_2 add remote

--- src/test/java/pageObject/TestFormPageAllure.java
+++ src/test/java/pageObject/TestFormPageAllure.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pageObject.base.TestBase;
 import pageObject.page.RegistrationPage;
@@ -14,6 +15,7 @@ import static io.qameta.allure.Allure.step;
 public class TestFormPageAllure extends TestBase {
 
     @Test
+    @Tag("less")
     @Feature("Форма регистрации")
     @Story("Заполнение формы")
     @Owner("Sergey")
--- src/test/java/pageObject/base/TestBase.java
+++ src/test/java/pageObject/base/TestBase.java
@@ -1,12 +1,13 @@
 package pageObject.base;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         //Configuration.pageLoadTimeout = 70000;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
